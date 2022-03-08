2e1c566150c5b848c2cd88e8f1ef4f422ddcc605
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-08 16:17:31 -0500

    fixed bugs and updated variable names to make more sense

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
 var apiKey = "9f0df682c222a2ec554b6d457b088d3d";
 
-var cityInput = document.getElementById('cityInput')
+containerEl = document.querySelector('container')
+containerEl.classList.add('row');
 
-var searchButton = document.getElementById('searchBtn')
-
+var API = 'dbadba3b6a415a81ba40263bf08007ee';
 var openCallWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?';
-var geoCallUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityInput.value + "&appid=" + apiKey
+var geoCodeWeatherUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=';
 
-var mainEL = document.querySelector('#mainShowing')
+var header = document.createElement('header');
+header.classList.add('col-12', 'display-1', 'text-center', 'py-5', 'bg-secondary', 'text-light');
+containerEl.appendChild(header);
+header.textContent = "Weather";
+
+var asideEl = document.createElement('aside');
+asideEl.classList.add('col-3', 'h5', 'py-5');
+containerEl.appendChild(asideEl);
+
+var mainEl = document.createElement('main');
+mainEl.classList.add('col-8', 'margin-main');
+containerEl.appendChild(mainEl);
+
+var currentWeatherEl = document.createElement('div')
+currentWeatherEl.classList.add('title', 'col-12','py-5', 'h2');
+mainEl.appendChild(currentWeatherEl);
+
+
+var fiveDayForecastEl = document.createElement('div');
+fiveDayForecastEl.classList.add('col-12', 'h3');
+fiveDayForecastEl.style.width = '8-rem;'
+mainEl.appendChild(fiveDayForecastEl);
+
+
+var searchEl = document.createElement('div');
+searchEl.textContent = 'Search for a City:'
+searchEl.classList.add('ms-4', 'mb-5')
+asideEl.appendChild(searchEl);
+
+var cityInput = document.createElement('input');
+cityInput.classList.add('col-12', 'mb-3', 'py-2');
+cityInput.placeholder = 'City'
+searchEl.appendChild(cityInput);
+
+var searchBtn = document.createElement('button');
+searchBtn.textContent = 'Search';
+searchBtn.classList.add('col-12','btn', 'btn-lg', 'btn-outline-secondary', 'px-5')
+searchBtn.type = 'button'
+searchEl.appendChild(searchBtn);
 
 var SearchCities = cityInput.value
 
@@ -48,7 +86,7 @@ var lon = weatherData[0].lon
 var coordinateRequestUrl = openCallWeatherUrl + 'lat=' + lat + '&lon=' + lon + '&appid=' + API + '&units=imperial';
 var weatherDisplay = function (weatherData, searchTerm) {
     if (weatherData.length === 0) {
-        mainEL.textContent = 'No data found';
+        mainEl.textContent = 'No data found';
         return;
     }
 
